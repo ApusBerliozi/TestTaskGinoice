16ba8b4bfc46ced43dcc68693ed176cebc9031ce
User <romanrabota602@gmail.com>
2023-09-23 00:45:26 +0600

    Corrected OpenApi visualisation

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
-from app import init_app
+from app import init_app, host, port
 
 if __name__ == "__main__":
-    init_app().run()
+    init_app().run(host=host,
+                   port=port)
--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from swagger_ui import api_doc
 from app.api_spec import CreateUserRequest, CreateTokenRequest, GetUserResponse
 
 db = SQLAlchemy()
+host = "localhost"
+port = 9000
 
 
 def init_app():
@@ -12,7 +14,7 @@ def init_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_pyfile('config/config.py')
     db.init_app(app)
-    api_doc(app, config_url='http://127.0.0.1:5000/api/spec', url_prefix='/api/doc', title='API doc')
+    api_doc(app, config_url=f'http://{host}:{port}/api/spec', url_prefix='/api/doc', title='API doc')
     with app.app_context():
         from . import endpoints
         db.create_all()
